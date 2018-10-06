9c13c287effe0cd1035217aecc7e01f2bedd6a71
Alex <dogrock2@Alexanders-MacBook-Pro.local>
2018-10-06 12:18:22 -0400

    added links functionality

--- server.py
+++ server.py
@@ -6,9 +6,33 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('navbar.html', title="Home")
+    return render_template('home.html', title="Home")
+
+@app.route("/settings")
+def settings():
+    return render_template('settings.html', title="Settings")
+
+@app.route("/register")
+def registration():
+    return render_template('register.html', title="Register")
 
 
+    
 if __name__ == "__main__":
     app.run(debug=True)
     # app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#source <desired-path>/bin/activate
